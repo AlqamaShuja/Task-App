f86eccc73d7a7726ef6c8cff0037e79fd62dfebc
AlqamaShuja <alqamashuja313@gmail.com>
2022-09-15 23:16:04 +0500

    sorting & pagination

--- src/model/user.js
+++ src/model/user.js
@@ -51,6 +51,8 @@ const userSchema = new mongoose.Schema({
             required: true
         }
     }]
+}, {
+    timestamps: true
 });
 
 userSchema.methods.generateAuthToken = async function () {
